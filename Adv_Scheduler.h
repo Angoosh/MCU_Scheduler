7b4d18a9c77e080c25133ed34a41f23a71e9cb12
Angoosh <kaplanantonin@gmail.com>
2022-03-17 22:54:56 +0100

    Period update function added
    Plus switched tabs for spaces

---
+++ Adv_Scheduler.h
@@ -0,0 +1,38 @@
+/*
+ * Adv_Scheduler.h
+ *
+ *  Created on: Mar 16, 2022
+ *      Author: angoosh
+ */
+
+#ifndef INC_ADV_SCHEDULER_H_
+#define INC_ADV_SCHEDULER_H_
+
+#include "stm32l0xx_hal.h"
+
+#define MAX_TASKS	32
+
+
+typedef struct {
+    uint32_t delay;
+    uint32_t period;
+    uint8_t run;
+    void (*exec)(void *handle);
+    void *handle;
+} tTask;
+
+typedef struct{
+    uint32_t cycles;//scheduler update cycles (for example in seconds)
+} Adv_Scheduler_Typedef;
+
+
+void Adv_Scheduler_Init();
+void Adv_Scheduler_Add_Task(void (*exec)(void *), void *handle, uint32_t delay, uint32_t period);
+int Adv_Scheduler_Remove_Task(void (*exec)(void *), void *handle);
+int Adv_Scheduler_Disable_Task(void (*exec)(void *), void *handle);
+int Adv_Scheduler_Enable_Task(void (*exec)(void *), void *handle);
+int Adv_Scheduler_Edit_Task_Period(void (*exec)(void *), void *handle, uint32_t period);
+void Adv_Scheduler_Update();
+void Adv_Scheduler_Exec();
+
+#endif /* INC_ADV_SCHEDULER_H_ */
